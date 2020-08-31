01654aa6f7d0918fa7563e06ee6e99413c0c7588
furyParrot <418396075@qq.com>
2020-08-31 11:23:40 +0800

    1

--- lib/RBTree/RBTree.h
+++ lib/RBTree/RBTree.h
@@ -4,7 +4,7 @@
 Node * CreatRBTree();
 int AddNode(Node* newnode);
 int RemoveNode(Node * node);
-Node * FindNode(int value);
+Node * FindNode(int keyvalue);
 int SaveRBTree(char * filename , char* (*tostring)(Node* a));
 Node* LoadRBTree(char * filename, Node* (*byScanf)(char* s));
 Node** GetAllNodes(Node* root);
