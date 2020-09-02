62f0561d3d87466a32d0dac657d00642a1f720be
furyParrot <418396075@qq.com>
2020-09-02 19:33:04 +0800

    2020年09月02日19:33:02

---
+++ lib/ClockMachine/ClockMachine.c
@@ -0,0 +1,20 @@
+#include "ClockMachine.h"
+
+int AddEmployee(Stru_BaseInfo a){
+    
+}
+int DelEmployee(int id){
+
+}
+int ClockIn(int id ){
+
+}
+int SaveFile(TableOfOneWeek* a){
+
+}
+int LoadFile(char* filename){
+
+}
+Stru_FullInfo** GetAllClockInfo(Node* root){
+
+}
